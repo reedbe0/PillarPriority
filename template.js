2297d6b07c561d453a1f597baa34a63c1ede88b0
Sebastian-Bock <bockse@oregonstate.edu>
2023-05-12 09:32:23 -0700

    modularized header and footer
    if/when we want to change these elements, we now only need to change one file

---
+++ template.js
@@ -0,0 +1,11 @@
+(function() {
+	var header = document.getElementsByTagName("header")[0];
+	header.innerHTML = `
+		<h1>PILLAR  PRIORITY</h1>
+	`;
+	
+	var footer = document.getElementsByTagName("footer")[0];
+	footer.innerHTML = `
+        <p>@Team Obelisk</p>
+	`;
+})();
