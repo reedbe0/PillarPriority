0566ae87afaa414d8836160482d0df0c7bf60ba2
reedbe0 <reedbe@oregonstate.edu>
2023-05-14 16:55:56 -0700

    Added Admin login tab

--- template.js
+++ template.js
@@ -1,4 +1,6 @@
 (function() {
+	document.getElementById("admin").style.display = "none"
+
 	var header = document.getElementsByTagName("header")[0];
 	if(header) header.innerHTML = `
 		<h1><a id = "home-link" href = "./index.html">PILLAR PRIORITY</a></h1>
@@ -9,3 +11,17 @@
         <p>@Team Obelisk</p>
 	`;
 })();
+
+function OnUserPress() {
+	document.getElementById("admin").style.display = "none"
+	document.getElementById("user").style.display = "block"
+	document.getElementById("adminButton").style.backgroundColor = "#344b59"
+	document.getElementById("userButton").style.backgroundColor = "#66b5da"
+}
+
+function OnAdminPress() {
+	document.getElementById("admin").style.display = "block"
+	document.getElementById("user").style.display = "none"
+	document.getElementById("userButton").style.backgroundColor = "#344b59"
+	document.getElementById("adminButton").style.backgroundColor = "#66b5da"
+}
